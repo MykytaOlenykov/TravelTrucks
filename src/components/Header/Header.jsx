214a8ab22989269bf3c857f417a363f8ef9c0e52
Mykyta <nikita2001395@gmail.com>
2024-10-21 21:49:13 +0300

    add: Header, Layout and fonts

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import css from "./Header.module.css";
+import logo from "../../assets/images/logo.svg";
+
+const routes = [
+  { title: "Home", path: "/" },
+  { title: "Catalog", path: "/catalog" },
+];
+
+export default function Header() {
+  const navigate = useNavigate();
+
+  return (
+    <header className={css.header}>
+      <img
+        className={css.logo}
+        src={logo}
+        alt="Site logo"
+        onClick={() => navigate("/")}
+      />
+
+      <nav>
+        <ul className={css.list}>
+          {routes.map(({ path, title }) => (
+            <li key={path}>
+              <NavLink to={path} className={css.link}>
+                {title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
